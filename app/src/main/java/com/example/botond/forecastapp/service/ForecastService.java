4babaf116f133bcccf04f55253ecea1f3ed31092
Boti <konczbotondvagyok@gmail.com>
2018-06-04 22:29:15 +0300

    ForecastService url changed to use key constant

--- app/src/main/java/com/example/botond/forecastapp/service/ForecastService.java
+++ app/src/main/java/com/example/botond/forecastapp/service/ForecastService.java
@@ -7,10 +7,10 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface ForecastService {
-    String KEY = "d86c7b39b933a92d3c3710792d00dae0";
+    String DARKSKY_KEY = "d86c7b39b933a92d3c3710792d00dae0";
     String SERVICE_ENDPOINT = "https://api.darksky.net/forecast/";
 
-    @GET("d86c7b39b933a92d3c3710792d00dae0/{coords}")
+    @GET(DARKSKY_KEY+"/{coords}")
     Call<Forecast> getWeather(@Path("coords") String coords);
 
 

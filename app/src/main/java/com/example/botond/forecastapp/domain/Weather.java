15bcdf9c30e22e1877b2f6084ff6ed86f6d71d2b
Boti <konczbotondvagyok@gmail.com>
2018-06-04 22:22:28 +0300

    Weather attributes created

--- app/src/main/java/com/example/botond/forecastapp/domain/Weather.java
+++ app/src/main/java/com/example/botond/forecastapp/domain/Weather.java
@@ -2,6 +2,11 @@ package com.example.botond.forecastapp.domain;
 
 public class Weather {
     private String summary;
+    private String precipType;
+    private double humidity;
+    private double precipProbability;
+    private double temperature;
+    private double uvIndex;
 
     public String getSummary() {
         return summary;
@@ -9,5 +14,45 @@ public class Weather {
 
     public void setSummary(String summary) {
         this.summary = summary;
+    }
+
+    public String getPrecipType() {
+        return precipType;
+    }
+
+    public void setPrecipType(String precipType) {
+        this.precipType = precipType;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPrecipProbability() {
+        return precipProbability;
+    }
+
+    public void setPrecipProbability(double precipProbability) {
+        this.precipProbability = precipProbability;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getUvIndex() {
+        return uvIndex;
+    }
+
+    public void setUvIndex(double uvIndex) {
+        this.uvIndex = uvIndex;
     }
 }
